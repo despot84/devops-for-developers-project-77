b5d80e38fcf45fb48e84c97034c106435ee39348
despot84 <40593747+despot84@users.noreply.github.com>
2025-12-11 22:11:32 +0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,106 +1,92 @@
-variable "hyperv_host" {
-  description = "Hyper-V host address"
-  type        = string
-  default     = "localhost"
-}
-
-variable "hyperv_port" {
-  description = "Hyper-V WinRM port"
-  type        = number
-  default     = 5985
-}
-
-variable "hyperv_ssh_port" {
-  description = "SSH port"
-  type        = number
-  default     = 22
-}
-
-
-variable "hyperv_user" {
-  description = "Hyper-V username"
-  type        = string
-  default     = "ServiceAccount"
-}
-
-variable "hyperv_password" {
-  description = "Hyper-V password"
+variable "yc_token" {
+  description = "OAuth-token"
   type        = string
   sensitive   = true
-#   default     = null
 }
 
-variable "hyperv_https" {
-  description = "Use HTTPS for WinRM"
-  type        = bool
-  default     = false
+variable "yc_cloud_id" {
+  type      = string
+  sensitive = true
 }
 
-variable "hyperv_insecure" {
-  description = "Allow insecure WinRM connections"
-  type        = bool
-  default     = false
+variable "yc_folder_id" {
+  type      = string
+  sensitive = true
 }
 
-variable "hyperv_use_ntlm" {
-  description = "Use NTLM authentication"
-  type        = bool
-  default     = false
-}
-
-variable "hyperv_script_path" {
-  description = "Path to store scripts on Hyper-V host"
+variable "yc_zone" {
+  description = "availability zone"
   type        = string
-  default     = "C:/Temp/terraform"
+  sensitive   = true
 }
 
-variable "hyperv_timeout" {
-  description = "Timeout for Hyper-V operations"
+variable "yc_service_account_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "vm_login" {
+  description = "VM user login"
   type        = string
-  default     = "30s"
+  sensitive   = true
 }
 
-variable "vm_name" {
-  description = "Name of the virtual machine"
+variable "yc_os_image_id" {
+  description = "yandex cloud id for Operating System image"
   type        = string
-  default     = "terraform-vm"
+  sensitive   = false
 }
 
-variable "iso_path" {
-  description = "Path to ISO file for installation"
-  type        = string
-  default     = "S:\\Distrib\\OS\\ubuntu-24.04.3-desktop-amd64.iso"
+variable "ssh_pub_key_path" {
+  type      = string
+  sensitive = false
 }
 
-variable "vm_memory_gb" {
-  description = "VM memory in GB"
-  type        = number
-  default     = 2
+variable "db_name" {
+  type      = string
+  sensitive = true
 }
 
-variable "vm_cpu_count" {
-  description = "Number of virtual CPUs"
-  type        = number
-  default     = 2
+variable "db_user" {
+  type      = string
+  sensitive = true
 }
 
-variable "disk_size_gb" {
-  description = "System disk size in GB"
-  type        = number
-  default     = 40
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "tls_fullchain_pem_path" {
+  type      = string
+  sensitive = true
+}
+
+variable "tls_privkey_pem_path" {
+  type      = string
+  sensitive = true
+}
+
+variable "domain_name" {
+  type = string
 }
 
 variable "datadog_api_key" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "datadog_app_key" {
-  type        = string
-  sensitive   = true
+  type      = string
+  sensitive = true
 }
 
 variable "datadog_api_url" {
-  type        = string
-  default     = "https://api.datadoghq.eu"
+  type      = string
+  sensitive = true
+}
+
+variable "datadog_site" {
+  type      = string
+  sensitive = true
 }
