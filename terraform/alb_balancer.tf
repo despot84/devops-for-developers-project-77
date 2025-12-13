3072da96355d1eaadcfcec62a929593214440c58
despot84 <40593747+despot84@users.noreply.github.com>
2025-12-13 16:45:08 +0300

    Update alb_balancer.tf

--- terraform/alb_balancer.tf
+++ terraform/alb_balancer.tf
@@ -12,12 +12,12 @@ resource "yandex_alb_target_group" "devops-tg" {
 
   target {
     subnet_id    = yandex_vpc_subnet.devops-subnet.id
-    ip_address = yandex_compute_instance.app-server-1.network_interface.0.ip_address
+    ip_address = yandex_compute_instance.app-server[0].network_interface.0.ip_address
   }
 
   target {
     subnet_id    = yandex_vpc_subnet.devops-subnet.id
-    ip_address = yandex_compute_instance.app-server-2.network_interface.0.ip_address
+    ip_address = yandex_compute_instance.app-server[1].network_interface.0.ip_address
   }
 }
 
